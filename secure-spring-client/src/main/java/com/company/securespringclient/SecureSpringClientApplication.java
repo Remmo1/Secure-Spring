0e325c4fe71f1b3950af56f1095985fbb2dd0bbd
remmo <remigiusz.pisarski@onet.pl>
2022-02-25 15:02:32 +0100

    this will be my first secure application!

---
+++ secure-spring-client/src/main/java/com/company/securespringclient/SecureSpringClientApplication.java
@@ -0,0 +1,13 @@
+package com.company.securespringclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureSpringClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureSpringClientApplication.class, args);
+    }
+
+}
---
+++ secure-spring-client/src/test/java/com/company/securespringclient/SecureSpringClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.securespringclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureSpringClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
